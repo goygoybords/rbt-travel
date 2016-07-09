d58ff49fe7aa62e8029858fbade1533271098cdb
unknown <kevinseankho@yahoo,com>
2016-07-09 11:05:12 +0800

    our tours page updated

--- wp-content/themes/twentyfourteen/page-our-tours.php
+++ wp-content/themes/twentyfourteen/page-our-tours.php
@@ -11,15 +11,25 @@
         	<!--start container-->
             <div class="container clearfix">
             
-
+                 <?php        
+                        $options = array('cat' => '3', 'order' => 'DESC');
+                        $query = new WP_Query($options);
+                        while ( $query->have_posts() ) : $query->the_post();
+                            $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'full' );
+                            $id = get_the_ID();
+                            $description = get_post_meta($id, 'description'); 
+                            $price = get_post_meta($id, 'tour-price'); 
+                        ?>
         		<div class="grid_6">
-                	<!--start archivetour-->
+                    <!--start archivetour-->
                     <div class="archivetour yellow fade-left animate1">
                         
                         <!--left-->
                         <div class="leftarchivetour">
                         
-                            <a href="single-project.html"><img alt="" class="imgleftarchivetour opacity" src="<?php bloginfo('template_url');?>/img/tours/photos/img1.jpg"></a>
+                            <a href="single-project.html">
+                                <img alt="" class="imgleftarchivetour opacity" src="<?php echo $image_src[0]; ?>">
+                            </a>
                         
                             <div class="pricetitleleftarchivetour">
                                 
@@ -37,7 +47,9 @@
                         <div class="rightarchivetour">
                         
                             <div class="titledayarchivetour">
-                                <a href="single-project.html"><p class="titlearchivetour">BRAZIL</p></a>
+                                <a href="single-project.html">
+                                    <p class="titlearchivetour"><?php the_title(); ?></p>
+                                </a>
                             
                                 <div class="dayarchivetour">
                                     <p>15</p>
@@ -55,7 +67,7 @@
                                     <a title="Fly and Drive" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/plane.png"></a>
                                     <a title="Sun" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/sun.png"></a>
                                     <a title="Insurance included" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/insurance.png"></a>
-        							<a title="Nature" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/nature.png"></a>
+                                    <a title="Nature" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/nature.png"></a>
                                 
                                 </p>
                             
@@ -67,177 +79,8 @@
                     </div>
                     <!--end archivetour-->
                 </div>
-                
-                <div class="grid_6">
-                	<!--start archivetour-->
-                    <div class="archivetour orange fade-right animate1">
-                        
-                        <!--left-->
-                        <div class="leftarchivetour">
-                        
-                            <a href="single-project.html"><img alt="" class="imgleftarchivetour opacity" src="<?php bloginfo('template_url');?>/img/tours/photos/img2.jpg"></a>
-                        
-                            <div class="pricetitleleftarchivetour">
-                                
-                                <div class="priceleftarchivetour">
-                                    <p>1000 $</p>
-                                </div>
-                                <p class="titleleftarchivetour">Bryce canyon tour</p>
-                            </div>
-                        
-                        
-                        </div>
-                        <!--end left-->
-                        
-                        <!--right-->
-                        <div class="rightarchivetour">
-                        
-                            <div class="titledayarchivetour">
-                                <a href="single-project.html"><p class="titlearchivetour">WEST COST</p></a>
-                            
-                                <div class="dayarchivetour">
-                                    <p>10</p>
-                                <span>DAYS</span>
-                            
-                            </div>
-                            
-                            </div>
-                            
-                            <div class="descriptioniconsarchivetour">
-                            
-                                <p class="descriptionarchivetour">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus, elit vitae fermentum hendrerit, neque erat fringilla nibh.</p>
-                                <p class="iconsarchivetour">
-                                
-                                    <a title="Car" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/car.png"></a>
-                                    <a title="Fly and Drive" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/plane.png"></a>
-                                    <a title="Sun" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/sun.png"></a>
-                                    <a title="Insurance included" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/insurance.png"></a>
-        							<a title="Nature" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/nature.png"></a>
-                                
-                                </p>
-                            
-                            </div>
-                        
-                        </div>
-                        <!--end right-->
-                           
-                    </div>
-                    <!--end archivetour-->
-                </div>
-                
-                <div class="grid_6">
-                	<!--start archivetour-->
-                    <div class="archivetour red fade-left animate1">
-                        
-                        <!--left-->
-                        <div class="leftarchivetour">
-                        
-                            <a href="single-project.html"><img alt="" class="imgleftarchivetour opacity" src="<?php bloginfo('template_url');?>/img/tours/photos/img3.jpg"></a>
-                        
-                            <div class="pricetitleleftarchivetour">
-                                
-                                <div class="priceleftarchivetour">
-                                    <p>700 $</p>
-                                </div>
-                                <p class="titleleftarchivetour">Amazing tour</p>
-                            </div>
-                        
-                        
-                        </div>
-                        <!--end left-->
-                        
-                        <!--right-->
-                        <div class="rightarchivetour">
-                        
-                            <div class="titledayarchivetour">
-                                <a href="single-project.html"><p class="titlearchivetour">TURKEY</p></a>
-                            
-                                <div class="dayarchivetour">
-                                    <p>10</p>
-                                <span>DAYS</span>
-                            
-                            </div>
-                            
-                            </div>
-                            
-                            <div class="descriptioniconsarchivetour">
-                            
-                                <p class="descriptionarchivetour">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus, elit vitae fermentum hendrerit, neque erat fringilla nibh.</p>
-                                <p class="iconsarchivetour">
-                                
-                                    <a title="Car" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/car.png"></a>
-                                    <a title="Fly and Drive" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/plane.png"></a>
-                                    <a title="Sun" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/sun.png"></a>
-                                    <a title="Insurance included" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/insurance.png"></a>
-        							<a title="Nature" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/nature.png"></a>
-                                
-                                </p>
-                            
-                            </div>
-                        
-                        </div>
-                        <!--end right-->
-                           
-                    </div>
-                    <!--end archivetour-->
-                </div>
-                
-               
-                <div class="grid_6">
-                	<!--start archivetour-->
-                    <div class="archivetour blue fade-left animate1">
-                        
-                        <!--left-->
-                        <div class="leftarchivetour">
-                        
-                            <a href="single-project.html"><img alt="" class="imgleftarchivetour opacity" src="<?php bloginfo('template_url');?>/img/tours/photos/img5.jpg"></a>
-                        
-                            <div class="pricetitleleftarchivetour">
-                                
-                                <div class="priceleftarchivetour">
-                                    <p>1000 $</p>
-                                </div>
-                                <p class="titleleftarchivetour">Relax tour</p>
-                            </div>
-                        
-                        
-                        </div>
-                        <!--end left-->
-                        
-                        <!--right-->
-                        <div class="rightarchivetour">
-                        
-                            <div class="titledayarchivetour">
-                                <a href="single-project.html"><p class="titlearchivetour">ALASKA</p></a>
-                            
-                                <div class="dayarchivetour">
-                                    <p>10</p>
-                                <span>DAYS</span>
-                            
-                            </div>
-                            
-                            </div>
-                            
-                            <div class="descriptioniconsarchivetour">
-                            
-                                <p class="descriptionarchivetour">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus, elit vitae fermentum hendrerit, neque erat fringilla nibh.</p>
-                                <p class="iconsarchivetour">
-                                
-                                    <a title="Car" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/car.png"></a>
-                                    <a title="Fly and Drive" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/plane.png"></a>
-                                    <a title="Sun" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/sun.png"></a>
-                                    <a title="Insurance included" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/insurance.png"></a>
-        							<a title="Nature" class="tooltip" href="#"><img alt="" src="<?php bloginfo('template_url');?>/img/tours/icon/nature.png"></a>
-                                
-                                </p>
-                            </div>
-                        </div>
-                        <!--end right-->    
-                    </div>
-                    <!--end archivetour-->
-                </div>
-            </div>
             <!--end container--> 
+            <?php endwhile; ?>
             
         </section>
         <!--end internal page-->
